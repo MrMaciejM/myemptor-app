9e72a9895d99a0a40724bc15fb1adaede6cedf28
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-04-18 19:17:59 +0100

    added rendering prototype, but still in progress and testing phase

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ console.log(getStorage);
 // 2. Display saved information 
 function displayClientData() {
     console.log(getStorage.length);
-    displayData.insertAdjacentHTML("afterend", "\n    <div class=\"displayRow\">\n      <p>".concat(getStorage[0].Name, " ").concat(getStorage[0].Surname, "</p>\n      \n    </div>\n    "));
+    displayData.insertAdjacentHTML("beforeend", "\n        <div class=\"displayRowHeadings\">\n        <p>Name</p>\n        <p>Surname</p>\n        <p>Phone</p>\n        <p>Email</p>\n        <p>Application Date</p>\n        <p>Choice</p>\n        <p>Start Date</p>\n        <p>Current Status</p>\n        <p>Bank</p>\n        <p>Sum</p>\n        <p>Rate Expander100 %</p>\n        <p>Firm Income</p>\n        <p>My Payment</p>\n        <p>My Notes</p>\n        <p>Summary Acti.</p>\n        </div>");
+    for (var i = 0; i < getStorage.length; i++) {
+        displayData.insertAdjacentHTML("afterend", "\n        <div class=\"displayRow\">\n        <p>".concat(getStorage[i].Name, "</p>            \n        <p>").concat(getStorage[i].Surname, "</p>            \n        <p>").concat(getStorage[i].Phone, "</p>            \n        <p>").concat(getStorage[i].Email, "</p>            \n        <p>").concat(getStorage[i].AppDate, "</p>            \n        <p>").concat(getStorage[i].Choice, "</p>            \n        <p>").concat(getStorage[i].StartDate, "</p>            \n        <p>").concat(getStorage[i].CurrStatus, "</p>            \n        <p>").concat(getStorage[i].Bank, "</p>            \n        <p>").concat(getStorage[i].Sum, "</p>            \n        <p>").concat(getStorage[i].FirmIncome, "</p>            \n        <p>").concat(getStorage[i].MyNotes, "</p>            \n        <p>").concat(getStorage[i].SummaryActi, "</p>\n        </div>\n        "));
+    }
 }
 displayClientData();
 // APP LOGIC
--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,44 @@ console.log(getStorage);
 // 2. Display saved information 
 function displayClientData() {
     console.log(getStorage.length);
-    displayData.insertAdjacentHTML("afterend", `
-    <div class="displayRow">
-      <p>${getStorage[0].Name} ${getStorage[0].Surname}</p>
-      
-    </div>
-    `)
+    displayData.insertAdjacentHTML("beforeend", `
+        <div class="displayRowHeadings">
+        <p>Name</p>
+        <p>Surname</p>
+        <p>Phone</p>
+        <p>Email</p>
+        <p>Application Date</p>
+        <p>Choice</p>
+        <p>Start Date</p>
+        <p>Current Status</p>
+        <p>Bank</p>
+        <p>Sum</p>
+        <p>Rate Expander100 %</p>
+        <p>Firm Income</p>
+        <p>My Payment</p>
+        <p>My Notes</p>
+        <p>Summary Acti.</p>
+        </div>`)
+
+    for(let i = 0; i < getStorage.length; i++) {
+        displayData.insertAdjacentHTML("afterend", `
+        <div class="displayRow">
+        <p>${getStorage[i].Name}</p>            
+        <p>${getStorage[i].Surname}</p>            
+        <p>${getStorage[i].Phone}</p>            
+        <p>${getStorage[i].Email}</p>            
+        <p>${getStorage[i].AppDate}</p>            
+        <p>${getStorage[i].Choice}</p>            
+        <p>${getStorage[i].StartDate}</p>            
+        <p>${getStorage[i].CurrStatus}</p>            
+        <p>${getStorage[i].Bank}</p>            
+        <p>${getStorage[i].Sum}</p>            
+        <p>${getStorage[i].FirmIncome}</p>            
+        <p>${getStorage[i].MyNotes}</p>            
+        <p>${getStorage[i].SummaryActi}</p>
+        </div>
+        `)
+    }
 }
 displayClientData();
 
@@ -40,3 +72,5 @@ form?.addEventListener("submit", (e) => {
     // display the 
 
 })
+
+
