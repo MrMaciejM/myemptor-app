ebba158a5d41753dc2ba557a2001869ac2c1e5a2
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-04-18 16:25:48 +0100

    added base elements and form, finished testing form with two inputs and working on localStorage and rendering elements next

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-console.log("hello 2");
-console.log("hello 3");
-console.log("hello 4");
-console.log("hello 5");
+var form = document.getElementById("form");
+var inpName = document.getElementById("name");
+var inpSurname = document.getElementById("surname");
+var displayData = document.getElementById("displayDataSection");
+// 1. Create LocalStorage if none exist for the app
+var getStorage = JSON.parse(localStorage.getItem("myEmptor"));
+if (!getStorage) {
+    localStorage.setItem("myEmptor", JSON.stringify([]));
+}
+console.log("Storage");
+console.log(getStorage);
+// 2. Display saved information 
+function displayClientData() {
+    console.log(getStorage.length);
+    displayData.insertAdjacentHTML("afterend", "\n    <div class=\"displayRow\">\n      <p>".concat(getStorage[0].Name, " ").concat(getStorage[0].Surname, "</p>\n      \n    </div>\n    "));
+}
+displayClientData();
+// APP LOGIC
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var array = getStorage;
+    console.log(inpName.value);
+    console.log(inpSurname.value);
+    //get value and save to storage on submit
+    array.push({ "Name": inpName.value, "Surname": inpSurname.value });
+    console.log(array);
+    // display the 
+});
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,42 @@
-console.log("hello 2");
-console.log("hello 3");
-console.log("hello 4");
-console.log("hello 5");
+const form = document.getElementById("form") as HTMLFormElement;
+const inpName = document.getElementById("name") as HTMLInputElement;
+const inpSurname = document.getElementById("surname") as HTMLInputElement;
+
+const displayData = document.getElementById("displayDataSection") as HTMLElement;
+
+// 1. Create LocalStorage if none exist for the app
+const getStorage = JSON.parse(localStorage.getItem("myEmptor")!);
+if (!getStorage) {
+    localStorage.setItem("myEmptor", JSON.stringify([]))
+} 
+console.log("Storage");
+console.log(getStorage);
+
+
+// 2. Display saved information 
+function displayClientData() {
+    console.log(getStorage.length);
+    displayData.insertAdjacentHTML("afterend", `
+    <div class="displayRow">
+      <p>${getStorage[0].Name} ${getStorage[0].Surname}</p>
+      
+    </div>
+    `)
+}
+displayClientData();
+
+// APP LOGIC
+form?.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    let array = getStorage;
+
+    console.log(inpName.value);
+    console.log(inpSurname.value);
+    
+    //get value and save to storage on submit
+    array.push({"Name": inpName.value,"Surname": inpSurname.value})
+    console.log(array);
+    
+    // display the 
+
+})
