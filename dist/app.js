bd2becc9a0ae6a3f1bacb8be99cfe84d33489a08
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-04-21 19:57:55 +0100

    compiled project using tsc

--- dist/app.js
+++ dist/app.js
@@ -1,49 +1,69 @@
+"use strict";
 // FORM variables
-var form = document.getElementById("form");
-var inpName = document.getElementById("name");
-var inpSurname = document.getElementById("surname");
-var inputFields = document.querySelectorAll("input, select");
+const form = document.getElementById("form");
+const inpName = document.getElementById("name");
+const inpSurname = document.getElementById("surname");
+const inputFields = document.querySelectorAll("input, select");
 // BUTTONS
-var deleteAllBtn = document.getElementById("deleteAllBtn");
-var deleteBtnYes = document.getElementById("deleteBtnYes");
-var deleteBtnNo = document.getElementById("deleteBtnNo");
-var clearFieldsBtn = document.getElementById("clearBtn");
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+const deleteBtnYes = document.getElementById("deleteBtnYes");
+const deleteBtnNo = document.getElementById("deleteBtnNo");
+const clearFieldsBtn = document.getElementById("clearBtn");
 // MODAL
-var deleteModal = document.getElementById("deleteModal");
-var modalCloseBtn = document.getElementById("modalCloseBtn");
+const deleteModal = document.getElementById("deleteModal");
+const modalCloseBtn = document.getElementById("modalCloseBtn");
 // DATA RENDERING
-var displayData = document.getElementById("displayDataSection");
-var displayRows = document.querySelectorAll(".displayRow");
+const displayData = document.getElementById("displayDataSection");
+const displayRows = document.querySelectorAll(".displayRow");
 // SORTING OPTIONS
-var sortOptions = document.getElementById("sortOptions");
+const sortOptions = document.getElementById("sortOptions");
 // 1. Create LocalStorage if none exist for the app
-var getStorage = JSON.parse(localStorage.getItem("myEmptor"));
+const getStorage = JSON.parse(localStorage.getItem("myEmptor"));
 if (!getStorage) {
     localStorage.setItem("myEmptor", JSON.stringify([]));
 }
 // 2. Display saved information/client data
 function displayClientData() {
-    var storageData = JSON.parse(localStorage.getItem("myEmptor")) || [];
+    const storageData = JSON.parse(localStorage.getItem("myEmptor")) || [];
     displayData.innerHTML = "";
-    storageData.forEach(function (item) {
-        displayData.insertAdjacentHTML("beforeend", "\n            <div class=\"displayRow\">\n            <p>".concat(item.Name, "</p>            \n            <p>").concat(item.Surname, "</p>            \n            <p>").concat(item.Phone, "</p>            \n            <p>").concat(item.Email, "</p>            \n            <p>").concat(item.AppDate, "</p>            \n            <p>").concat(item.Choice, "</p>            \n            <p>").concat(item.StartDate, "</p>            \n            <p>").concat(item.CurrStatus, "</p>            \n            <p>").concat(item.Bank, "</p>            \n            <p>").concat(item.Sum, "</p>            \n            <p>").concat(item.RateExp, "</p>            \n            <p>").concat(item.FirmIncome, "</p>            \n            <p>").concat(item.MyPayment, "</p>            \n            <p>").concat(item.MyNotes, "</p>            \n            <p>").concat(item.SummaryActi, "</p>\n            <button class=\"delClientBtn\" id=").concat(item.id, ">X</button>\n            </div>\n        "));
+    storageData.forEach(item => {
+        displayData.insertAdjacentHTML("beforeend", `
+            <div class="displayRow">
+            <p>${item.Name}</p>            
+            <p>${item.Surname}</p>            
+            <p>${item.Phone}</p>            
+            <p>${item.Email}</p>            
+            <p>${item.AppDate}</p>            
+            <p>${item.Choice}</p>            
+            <p>${item.StartDate}</p>            
+            <p>${item.CurrStatus}</p>            
+            <p>${item.Bank}</p>            
+            <p>${item.Sum}</p>            
+            <p>${item.RateExp}</p>            
+            <p>${item.FirmIncome}</p>            
+            <p>${item.MyPayment}</p>            
+            <p>${item.MyNotes}</p>            
+            <p>${item.SummaryActi}</p>
+            <button class="delClientBtn" id=${item.id}>X</button>
+            </div>
+        `);
     });
 }
 displayClientData();
 // Form Submission Logic
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (e) {
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
     e.preventDefault();
-    var array = getStorage;
+    let array = getStorage;
     // prettier-ignore
-    var fieldNames = ["Name", "Surname", "Phone", "Email", "AppDate", "Choice", "StartDate", "CurrStatus", "Bank", "Sum", "RateExp", "FirmIncome", "MyPayment", "MyNotes", "SummaryActi", "id"];
-    var clientData = {};
+    const fieldNames = ["Name", "Surname", "Phone", "Email", "AppDate", "Choice", "StartDate", "CurrStatus", "Bank", "Sum", "RateExp", "FirmIncome", "MyPayment", "MyNotes", "SummaryActi", "id"];
+    const clientData = {};
     // preparing data to be stored in key value pairs {"fieldNames[i]": "userInputs.value"} and pushing to localStorage
-    for (var i = 0; i < 16; i++) {
-        var userInputs = inputFields[i];
+    for (let i = 0; i < 16; i++) {
+        const userInputs = inputFields[i];
         clientData[fieldNames[i]] = userInputs.value;
     }
     // sets ID on each object - forgot to do it initially, so the ID is in the last place to avoid breaking storing and retreiving objects
-    var counter = getStorage.length;
+    let counter = getStorage.length;
     clientData.id = counter;
     array.push(clientData);
     localStorage.setItem("myEmptor", JSON.stringify(array));
@@ -54,37 +74,37 @@ function hideModal() {
 }
 // 3. DELETE ALL FUNCTIONALITY
 // Bring up the modal on Delete All click
-deleteAllBtn.addEventListener("click", function (e) {
+deleteAllBtn.addEventListener("click", (e) => {
     deleteModal.classList.remove("hide");
 });
 // Close Modal on clicking X
-modalCloseBtn.addEventListener("click", function (e) {
+modalCloseBtn.addEventListener("click", (e) => {
     hideModal();
 });
 // Wipe ALL Data
-deleteBtnYes.addEventListener("click", function (e) {
+deleteBtnYes.addEventListener("click", (e) => {
     localStorage.setItem("myEmptor", JSON.stringify([]));
-    var displayRow = document.getElementById("displayRow");
+    const displayRow = document.getElementById("displayRow");
     displayRow === null || displayRow === void 0 ? void 0 : displayRow.remove();
     hideModal();
     location.reload();
 });
 // Select "No" option
-deleteBtnNo.addEventListener("click", function (e) {
+deleteBtnNo.addEventListener("click", (e) => {
     hideModal();
 });
 // 4. CLEAR FIELDS FUNCTIONALITY
-clearFieldsBtn.addEventListener("click", function (e) {
-    inputFields.forEach(function (field) {
+clearFieldsBtn.addEventListener("click", (e) => {
+    inputFields.forEach(field => {
         field.value = "";
     });
 });
 // 5. SORTING FUNCTIONALITY 
-sortOptions.addEventListener("click", function (e) {
-    var selectedOption = e.target.value;
+sortOptions.addEventListener("click", (e) => {
+    const selectedOption = e.target.value;
     //console.log("sort by name is: " + selectedOption);
     // localStorage data which will be used for the filter
-    var dataArray = getStorage.map(function (item) {
+    const dataArray = getStorage.map(item => {
         return {
             Name: item.Name,
             Surname: item.Surname,
@@ -106,77 +126,77 @@ sortOptions.addEventListener("click", function (e) {
     // sorting logic 
     switch (selectedOption) {
         case "name-AZ":
-            dataArray.sort(function (a, b) { return a.Name.localeCompare(b.Name); });
+            dataArray.sort((a, b) => a.Name.localeCompare(b.Name));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "name-ZA":
             //displayRows.forEach(row => row.remove());
-            dataArray.sort(function (a, b) { return b.Name.localeCompare(a.Name); });
+            dataArray.sort((a, b) => b.Name.localeCompare(a.Name));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "surname-AZ":
-            dataArray.sort(function (a, b) { return a.Surname.localeCompare(b.Surname); });
+            dataArray.sort((a, b) => a.Surname.localeCompare(b.Surname));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "surname-ZA":
-            dataArray.sort(function (a, b) { return b.Surname.localeCompare(a.Surname); });
+            dataArray.sort((a, b) => b.Surname.localeCompare(a.Surname));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "applicationDateNew":
-            dataArray.sort(function (a, b) { return Date.parse(b.AppDate) - Date.parse(a.AppDate); });
+            dataArray.sort((a, b) => Date.parse(b.AppDate) - Date.parse(a.AppDate));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "applicationDateOld":
-            dataArray.sort(function (a, b) { return Date.parse(a.AppDate) - Date.parse(b.AppDate); });
+            dataArray.sort((a, b) => Date.parse(a.AppDate) - Date.parse(b.AppDate));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "bank":
-            dataArray.sort(function (a, b) { return b.Bank.localeCompare(a.Bank); });
+            dataArray.sort((a, b) => b.Bank.localeCompare(a.Bank));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "sum":
-            dataArray.sort(function (a, b) { return parseFloat(b.Sum) - parseFloat(a.Sum); });
+            dataArray.sort((a, b) => parseFloat(b.Sum) - parseFloat(a.Sum));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "firmIncomeLow":
-            dataArray.sort(function (a, b) { return parseFloat(a.FirmIncome) - parseFloat(b.FirmIncome); });
+            dataArray.sort((a, b) => parseFloat(a.FirmIncome) - parseFloat(b.FirmIncome));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "firmIncomeHigh":
-            dataArray.sort(function (a, b) { return parseFloat(b.FirmIncome) - parseFloat(a.FirmIncome); });
+            dataArray.sort((a, b) => parseFloat(b.FirmIncome) - parseFloat(a.FirmIncome));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "myPaymentLow":
-            dataArray.sort(function (a, b) { return parseFloat(a.MyPayment) - parseFloat(b.MyPayment); });
+            dataArray.sort((a, b) => parseFloat(a.MyPayment) - parseFloat(b.MyPayment));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
         case "myPaymentHigh":
-            dataArray.sort(function (a, b) { return parseFloat(b.MyPayment) - parseFloat(a.MyPayment); });
+            dataArray.sort((a, b) => parseFloat(b.MyPayment) - parseFloat(a.MyPayment));
             localStorage.setItem("myEmptor", JSON.stringify(dataArray));
             displayClientData();
             break;
     }
 }); // end of sorting function 
 // 6. REMOVE SINGLE CLIENT ON MOUSE CLICK + CONFIRMATION
-var deleteClientBtn = document.getElementsByClassName("delClientBtn");
-for (var i = 0; i < deleteClientBtn.length; i++) {
-    displayData.addEventListener("click", function (e) {
-        var getId = e.target.getAttribute("id");
+const deleteClientBtn = document.getElementsByClassName("delClientBtn");
+for (let i = 0; i < deleteClientBtn.length; i++) {
+    displayData.addEventListener("click", (e) => {
+        let getId = e.target.getAttribute("id");
         getId = parseInt(getId);
         //console.log("type of getId:" + typeof(getId));        
-        var clientData = JSON.parse(localStorage.getItem("myEmptor"));
-        var itemIndex = clientData.findIndex(function (index) { return index.id === getId; });
+        const clientData = JSON.parse(localStorage.getItem("myEmptor"));
+        const itemIndex = clientData.findIndex(index => index.id === getId);
         if (itemIndex !== -1) {
             clientData.splice(itemIndex, 1);
             localStorage.setItem("myEmptor", JSON.stringify(clientData));
@@ -187,45 +207,45 @@ for (var i = 0; i < deleteClientBtn.length; i++) {
 }
 ;
 // 7. BACKUP OF DATA AND SHOWING BACKUP TIME
-var backupBtn = document.getElementById("backupBtn");
+const backupBtn = document.getElementById("backupBtn");
 if (!localStorage.getItem("myEmptorTime")) {
     localStorage.setItem("myEmptorTime", JSON.stringify(""));
 }
 ;
 function showLastBackupTime() {
-    var currentDate = new Date();
-    var day = currentDate.getDate();
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var monthName = monthNames[currentDate.getMonth()];
-    var year = currentDate.getFullYear();
-    var hours = currentDate.getHours();
-    var minutes = currentDate.getMinutes().toString().padStart(2, "0");
-    var formattedDate = "".concat(day, "-").concat(monthName, "-").concat(year, " ").concat(hours, ":").concat(minutes);
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthName = monthNames[currentDate.getMonth()];
+    const year = currentDate.getFullYear();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+    const formattedDate = `${day}-${monthName}-${year} ${hours}:${minutes}`;
     console.log(formattedDate);
     return formattedDate;
 }
-var updateTime = document.getElementById("backupTimeParagraph");
-var getTime = localStorage.getItem("myEmptorTime");
+const updateTime = document.getElementById("backupTimeParagraph");
+const getTime = localStorage.getItem("myEmptorTime");
 // sets the p tag with last backup time
-updateTime === null || updateTime === void 0 ? void 0 : updateTime.textContent = "Last backup was on ".concat(getTime === null || getTime === void 0 ? void 0 : getTime.replaceAll('"', ""));
+updateTime === null || updateTime === void 0 ? void 0 : updateTime.textContent = `Last backup was on ${getTime === null || getTime === void 0 ? void 0 : getTime.replaceAll('"', "")}`;
 // update time on click, and take backup.
-var getBackupData = localStorage.getItem("myEmptorBackup");
+const getBackupData = localStorage.getItem("myEmptorBackup");
 if (!getBackupData) {
     localStorage.setItem("myEmptorBackup", JSON.stringify([]));
 }
-backupBtn === null || backupBtn === void 0 ? void 0 : backupBtn.addEventListener("click", function (e) {
+backupBtn === null || backupBtn === void 0 ? void 0 : backupBtn.addEventListener("click", (e) => {
     localStorage.setItem("myEmptorTime", JSON.stringify(showLastBackupTime()));
-    updateTime === null || updateTime === void 0 ? void 0 : updateTime.textContent = "Last backup was on ".concat(getTime === null || getTime === void 0 ? void 0 : getTime.replaceAll('"', ""));
-    var backupBtnDiv = document.getElementById("backupBtnDiv");
+    updateTime === null || updateTime === void 0 ? void 0 : updateTime.textContent = `Last backup was on ${getTime === null || getTime === void 0 ? void 0 : getTime.replaceAll('"', "")}`;
+    const backupBtnDiv = document.getElementById("backupBtnDiv");
     localStorage.setItem("myEmptorBackup", JSON.stringify(getStorage));
     location.reload();
 });
 showLastBackupTime();
 // 8. RESTORE FROM BACKUP
-var restoreBtn = document.getElementById("restoreBtn");
-restoreBtn === null || restoreBtn === void 0 ? void 0 : restoreBtn.addEventListener("click", function (e) {
+const restoreBtn = document.getElementById("restoreBtn");
+restoreBtn === null || restoreBtn === void 0 ? void 0 : restoreBtn.addEventListener("click", (e) => {
     console.log("clicked");
-    var emptorBackup = JSON.parse(localStorage.getItem("myEmptorBackup"));
+    const emptorBackup = JSON.parse(localStorage.getItem("myEmptorBackup"));
     localStorage.setItem("myEmptor", JSON.stringify(emptorBackup));
     displayClientData();
     console.log(emptorBackup);
@@ -239,3 +259,4 @@ restoreBtn === null || restoreBtn === void 0 ? void 0 : restoreBtn.addEventListe
 // .then(data => console.log(data))
 // .catch(error => console.error(error));
 // NOTE: most likely will scratch the fetch stock idea because of relevancy, though it is still TBC. 
+//# sourceMappingURL=app.js.map
